f715c8c09c96896a4c589e0e62227f57d8aa4840
yamatatsu <the.kinnikuman@gmail.com>
2017-05-25 00:34:30 +0900

    hot module replacement

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { AppContainer } from 'react-hot-loader'
 
 import reducer from './reducers'
 import App from './containers/app_container'
@@ -11,9 +12,21 @@ injectTapEventPlugin()
 
 const store = createStore(reducer)
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    </AppContainer>,
+    document.getElementById('root'),
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./containers/app_container', () => {
+    render(App)
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
-const path = require('path')
+const { resolve } = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: [
+    // for hot module replacement
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+
+    './src/index.js',
+  ],
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -18,4 +27,16 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  devServer: {
+    contentBase: resolve(__dirname, 'dist'),
+    publicPath: '/',
+    hot: true,
+  },
+
 }
